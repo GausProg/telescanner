ec502e347a1af676346faf4c8b68c620a2eff1fb
GausProg <60312483+GausProg@users.noreply.github.com>
2023-11-28 14:32:40 +0500

    Add files via upload

---
+++ telegrambotscript.py
@@ -0,0 +1,106 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telebot import types
+import zipfile
+
+balance = 20
+
+# инициализация логгера
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+# функция для обработки команды start
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Привет! Ваш баланс: {balance}")
+
+'''
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Узнать баланс")
+    btn2 = types.KeyboardButton("Пополнить баланс")
+    markup.add(btn1, btn2)
+'''
+# функция для обработки команды account
+def account(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Ваш баланс: {balance}")
+
+
+# функция для обработки сообщений с текстом
+def text_handler(update, context):
+    global balance
+    text = update.message.text
+    if text == 'Узнать баланс':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Ваш баланс: {balance}")
+    elif text == 'Пополнить баланс':
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Баланс пополнен на 10 единиц")
+        balance = balance + 10
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Неверная команда")
+
+
+# функция для обработки сообщений с фотографиями
+def photo_handler(update, context):
+    global balance
+    if balance <= 0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Ваш баланс равен нулю, пополните баланс")
+        return
+
+    photos = []
+    for photo in update.message.photo:
+        file_info = context.bot.get_file(photo.file_id)
+        downloaded_file = context.bot.download_file(file_info.file_path)
+        photos.append(downloaded_file)
+
+
+    '''photo = update.message.photo[-1].get_file()
+    ourphoto = photo.download('image.jpeg')'''
+
+    handler = zipfile.ZipFile("PhotoZip.zip", "w")
+    for ph in photos:
+        handler.write(ph)
+    handler.close()
+
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open('PhotoZip.zip', 'rb'))
+
+    balance = balance - 10
+
+
+
+# функция для обработки команды help
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Список команд:\n/start - Начать\n/account - Узнать баланс\n/pay - Пополнить баланс")
+
+def pay(update, context):
+    global balance
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Баланс пополнен на 10 единиц")
+    balance = balance + 10
+
+
+# функция для обработки неизвестных команд
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Неверная команда")
+
+
+# создание экземпляра Updater и добавление обработчиков
+updater = Updater(token='6422490098:AAE2-XMqDV3CHUnF68lNDJAR7atZEk3dX58', use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+account_handler = CommandHandler('account', account)
+text_handler = MessageHandler(Filters.text & (~Filters.command), text_handler)
+photo_handler = MessageHandler(Filters.photo, photo_handler)
+help_handler = CommandHandler('help', help)
+pay_handler = CommandHandler('pay', pay)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(account_handler)
+dispatcher.add_handler(text_handler)
+dispatcher.add_handler(photo_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(pay_handler)
+dispatcher.add_handler(unknown_handler)
+
+# запуск бота
+updater.start_polling()
